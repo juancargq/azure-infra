8b1da4fdb7899499db6b349bbf9811b4262f704c
juancargq <jcgomezquint@hotmail.com>
2025-02-26 20:24:52 +0100

    se añade .gitignore para Terraform

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Se define el proveedor de Azure
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Se define el grupo de recursos
+resource "azurerm_resource_group" "resource_group" {
+  name     = "RG-CP2"
+  location = "West Europe"
+}
+
+# Se define el conjunto de disponibilidad para las máquinas virtuales
+resource "azurerm_availability_set" "availability_set" {
+  name                = "AS-CP2"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
